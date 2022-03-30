3af4b88e9a6c16ce9cb3a3080f3fcb908f8eab70
JohnGupta-She <jgupta-she30@stuy.edu>
2022-03-30 14:04:19 -0400

    foiajeogiajg

---
+++ 85_stack/LLStack.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+
+public class LLStack<PANCAKE>{
+  private LinkedList<PANCAKE> who;
+
+  public LLStack(){
+    who = new LinkedList<PANCAKE>();
+  }
+
+  public boolean isEmpty(){
+    return (who.size() == 0);
+  }
+
+  public PANCAKE peekTop(){
+    return who.getFirst();
+  }
+
+  public PANCAKE pop(){
+    PANCAKE out = peekTop();
+    who.removeFirst();
+    return out;
+  }
+
+  public void push(PANCAKE bob){
+    who.add(0,bob);
+  }
+}
